7fee55051c7f8b93e8380393059f81192092f810
leeyc924 <leeyc924@naver.com>
2020-07-15 21:23:03 +0900

    xml dao에 옮기기

--- Spring_Exer/src/main/java/com/leeyc/study/board/dao/BoardDao.java
+++ Spring_Exer/src/main/java/com/leeyc/study/board/dao/BoardDao.java
@@ -2,12 +2,17 @@ package com.leeyc.study.board.dao;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.leeyc.study.board.vo.BoardVO;
 
-public interface BoardDao {
-	//게시글 작성
+@Repository
+public interface BoardDao{
+	
+	// 게시글 작성
 	public void write(BoardVO boardVO) throws Exception;
 	
-	//게시글 목록
+	// 게시글 목록
 	public List<BoardVO> list() throws Exception;
+	
 }
